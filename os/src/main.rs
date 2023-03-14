16f10b127d7f62c142514edad0e1fff1dc2a7d4e
raspirin <f1193968991@126.com>
2023-03-14 15:33:39 +0800

    finish ch1

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
+#![no_std]
+#![no_main]
+#![feature(panic_info_message)]
+
+mod console;
+mod lang_items;
+mod sbi;
+
+use core::arch::{asm, global_asm};
+use crate::sbi::shutdown;
+global_asm!(include_str!("entry.asm"));
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
     println!("Hello, world!");
+    shutdown();
+    //loop {}
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
 }
