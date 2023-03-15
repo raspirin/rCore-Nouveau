bf289b351e31de173587bc9961acf7ac1c816f01
raspirin <f1193968991@126.com>
2023-03-15 11:56:52 +0800

    some clean up

--- os/src/main.rs
+++ os/src/main.rs
@@ -9,21 +9,14 @@ mod sbi;
 
 use crate::sbi::shutdown;
 use core::arch::global_asm;
-use log::{debug, error, info, trace, warn};
+
 global_asm!(include_str!("entry.asm"));
 
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
     logging::init();
-    println!("Hello, world!");
-    warn!("This is a warn message.");
-    error!("This is an error message.");
-    debug!("This is a debug message.");
-    info!("This is an info message");
-    trace!("This is an trace message");
     shutdown();
-    //loop {}
 }
 
 fn clear_bss() {
