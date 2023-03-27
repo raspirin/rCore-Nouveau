3542e16588317c8536ee0ac06688dfdc710c1770
raspirin <f1193968991@126.com>
2023-03-27 21:16:42 +0800

    make file

---
+++ user/src/bin/01store_fault.rs
@@ -0,0 +1,15 @@
+#![no_std]
+#![no_main]
+
+#[macro_use]
+extern crate user_lib;
+
+#[no_mangle]
+fn main() -> i32 {
+    println!("Into Test store_fault, we will insert a invalid store operation.");
+    println!("Kernel should kill this program!");
+    unsafe {
+        core::ptr::null_mut::<u8>().write_volatile(0);
+    }
+    0
+}
