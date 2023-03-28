2b00e1b30ffab5c64977f9b09824df314cd50e55
raspirin <f1193968991@126.com>
2023-03-28 10:42:09 +0800

    user mode programs with qemu-riscv64

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -23,8 +23,10 @@ fn main() -> i32 {
     panic!("Can't find main!");
 }
 
+#[no_mangle]
+#[link_section = ".text.entry"]
 pub extern "C" fn _start() -> ! {
-    clear_bss();
+    // clear_bss();
     exit(main());
     panic!("unreachable after sys_exit!");
 }
