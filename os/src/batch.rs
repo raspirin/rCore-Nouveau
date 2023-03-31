d0d836d4032a88600d454b2376906ec8179bdad7
raspirin <f1193968991@126.com>
2023-03-31 11:40:52 +0800

    fix kernel stack size and resize user stack

--- os/src/batch.rs
+++ os/src/batch.rs
@@ -7,8 +7,8 @@ use lazy_static::lazy_static;
 const MAX_APP_NUM: usize = 16;
 const APP_BASE_ADDRESS: usize = 0x80400000;
 const APP_SIZE_LIMIT: usize = 0x20000;
-const USER_STACK_SIZE: usize = 4096 * 2;
-const KERNEL_STACK_SIZE: usize = 4097 * 2;
+const USER_STACK_SIZE: usize = 4096;
+const KERNEL_STACK_SIZE: usize = 4096 * 2;
 
 #[repr(align(4096))]
 struct KernelStack {
